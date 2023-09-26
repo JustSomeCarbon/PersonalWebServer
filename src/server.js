b03acce87c222a9243d9968bb7075531e379a1b3
Noe Garcia <noe.garcia@student.nmt.edu>
2023-09-26 10:22:15 -0600

    feat: populate landing page

--- src/server.js
+++ src/server.js
@@ -7,10 +7,27 @@ app.use(express.static(path.join(__dirname, 'frontend/')));
 
 const port = 3000;
 
+
+/*
+ * Routing functions
+ */
+
 app.get('/', (req, res) => {
-    //res.sendFile(path.join(__dirname, 'frontend/index.html'));
-    res.sendFile('frontend/index.html');
+    res.sendFile('index.html');
 });
+
+app.get('/background', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/background.html'));
+});
+
+app.get('/projects', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/projects.html'));
+});
+
+
+/*
+ * Server listen function
+ */
 
 app.listen(port, () => {
     console.log(`Server listening at localhost:${port}/`);
